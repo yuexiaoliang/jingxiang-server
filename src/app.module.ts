9beb905e1b3d86fd150a3eca03abc8b1a2325f3a
岳晓亮 <2220124666@qq.com>
2023-07-27 18:06:12 +0800

    feat: TypeORM 基本配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from 'config/configuration';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const db = configService.get('db');
+        return {
+          type: db.type,
+          host: db.host,
+          port: db.port,
+          username: db.username,
+          password: db.password,
+          database: db.database,
+          entities: [],
+          synchronize: db.synchronize,
+        };
+      },
     }),
 
     UserModule,
